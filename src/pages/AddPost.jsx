85821a7c82985f7de40bd948d517a5a29c72f5f5
Utkarsh Raj Mishra <utkarshrajmishra811545@gmail.com>
2024-04-30 22:46:26 +0530

    Implement Add Post

---
+++ src/pages/AddPost.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Container from '../components/container/Container';
+import Postform from '../components/PostForm/Postform';
+const AddPost=()=>{
+    return(
+        <div className='py-8'>
+            <Container>
+                <Postform/>
+            </Container>
+        </div>
+    )
+}
